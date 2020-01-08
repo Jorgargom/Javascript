1d69a8c3bf385cd4c95bf38741974682c4cb932b
Jorge García Gómez <jorge@jorgegarciagomez.es>
2020-01-08 08:03:44 +0100

    selecciono un unico elemento

--- fundamentos/universidad/js/app.js
+++ fundamentos/universidad/js/app.js
@@ -1,26 +1,20 @@
-// getElementbyId
+// Query Selector
 
-let encabezado;
+const encabezado = document.querySelector('.encabezado');
+const encabezadoAll = document.querySelectorAll('.enlace');
+const encabezadoH1 = document.querySelectorAll('h1');
 
-encabezado = document.getElementById('encabezado');
-
-encabezado.style.background = '#333';
-encabezado.style.color = 'white';
-encabezado.style.padding = '20px';
-
-
-
-// Cambiar el texto
-
-encabezado.innerText = 'Los mejores Cursos';
-
-
-
+const encabezadoCard = document.querySelectorAll('.card img');
 
 console.log(encabezado);
+console.log(encabezadoAll);
+console.log(encabezadoH1);
+console.log(encabezadoCard);
 
 
+let enlace;
+
+enlace = document.querySelector('#principal a:nth-child(3)');
 
 
-// Eliminar de Local Storage
-localStorage.clear();
+console.log(enlace);
