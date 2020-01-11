4e1a945bc84f31392bfc78f50b1e89ad7d1114a2
Jorge García Gómez <jorge@jorgegarciagomez.es>
2020-01-11 22:24:53 +0100

    event listener varios

--- fundamentos/universidad/js/app.js
+++ fundamentos/universidad/js/app.js
@@ -1,12 +1,30 @@
-document.querySelector('#submit-buscador').addEventListener('click', ejecutarBoton);
+const encabezado = document.querySelector('#encabezado');
 
-function ejecutarBoton(e) {
-    e.preventDefault();
-    let elemento;
-    elemento = e.target.id;
+const enlaces = document.querySelectorAll('.enlace');
 
-    console.log(elemento);
+const boton = document.querySelector('#vaciar-carrito');
+
+
+boton.addEventListener('click', obtenerEvento);
+
+boton.addEventListener('dblclick', obtenerEvento);
+
+boton.addEventListener('mouseenter', obtenerEvento);
+
+boton.addEventListener('mouseleave', obtenerEvento);
+
+boton.addEventListener('mouseover', obtenerEvento); 
+
+boton.addEventListener('mouseout', obtenerEvento);
+
+boton.addEventListener('mousedown', obtenerEvento);
+
+boton.addEventListener('mouseup', obtenerEvento);
+
+encabezado.addEventListener('mousemove', obtenerEvento);
+
+function obtenerEvento(e) {
+    console.log(`EVENTO: ${e.type}`);
+        
 }
-
-
 
