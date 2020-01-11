1b6e7c5ba620077199df0b9682200a69a501fb59
Jorge García Gómez <jorge@jorgegarciagomez.es>
2020-01-11 22:17:04 +0100

    event listener - click

--- fundamentos/universidad/js/app.js
+++ fundamentos/universidad/js/app.js
@@ -1,34 +1,12 @@
-// Reemplazar elementos
+document.querySelector('#submit-buscador').addEventListener('click', ejecutarBoton);
 
-const nuevoEncabezado = document.createElement('h2');
+function ejecutarBoton(e) {
+    e.preventDefault();
+    let elemento;
+    elemento = e.target.id;
 
-// agregar una id
-nuevoEncabezado.id = 'encabezado';
-
-//agregar nuevo texto
-nuevoEncabezado.appendChild(document.createTextNode('Los mejores cursos'));
-
-// Elemento anterior será remplazado 
-
-const anterior = document.querySelector('#encabezado');
-
-const elementoPadre = document.querySelector('#lista-cursos');
-
-elementoPadre.replaceChild(nuevoEncabezado, anterior)
+    console.log(elemento);
+}
 
 
-console.log(elementoPadre);
-
-console.log(anterior);
-
-console.log(nuevoEncabezado);
-
-
-const enlaces = document.querySelectorAll('.enlace');
-const navegacion = document.querySelector('#principal');
-
-enlaces[0].remove();
-
-console.log(enlaces);
-console.log(navegacion);
 
