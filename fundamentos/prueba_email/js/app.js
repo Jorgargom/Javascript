2add3904c1046cc7283ad1795395fb8aa136e488
Jorge García Gómez <jorge@jorgegarciagomez.es>
2020-01-15 20:42:14 +0100

    valido campos del formulario con JS

--- fundamentos/prueba_email/js/app.js
+++ fundamentos/prueba_email/js/app.js
@@ -5,6 +5,7 @@ const asunto = document.getElementById('asunto');
 const mensaje = document.getElementById('mensaje');
 const btnEnviar = document.getElementById('enviar');
 const formularioEnviar = document.getElementById('enviar-mail');
+const resetBtn = document.getElementById('resetBtn');
 
 
 
@@ -16,6 +17,10 @@ function eventListeners() {
      // Inicio de la aplicación y deshabilitar submit
      document.addEventListener('DOMContentLoaded', inicioApp);
 
+    // Campos del formulario
+    email.addEventListener('blur', validarCampo);
+    asunto.addEventListener('blur', validarCampo);
+    mensaje.addEventListener('blur', validarCampo);
     
 }
 
@@ -28,3 +33,48 @@ function inicioApp() {
     btnEnviar.disabled = true;
 }
 
+
+// Valida que el campo tengo algo escrito
+
+function validarCampo() {
+    
+    // Se valida la longitud del texto y que no este vacio
+    validarLongitud(this);
+
+    // Validar unicamente el email
+    if(this.type === 'email') {
+         validarEmail(this);
+    }
+
+    let errores = document.querySelectorAll('.error');
+
+    if(email.value !== '' && asunto.value !== '' && mensaje.value !== '' ) {
+         if(errores.length === 0) {
+              btnEnviar.disabled = false;
+         }
+    }
+}
+
+
+// Verifica la longitud del texto en los campos
+function validarLongitud(campo) {
+
+    if(campo.value.length > 0 ) {
+         campo.style.borderBottomColor = 'green';
+         campo.classList.remove('error');
+    } else {
+         campo.style.borderBottomColor = 'red';
+         campo.classList.add('error');
+    }
+}
+
+function validarEmail(campo) {
+    const mensaje = campo.value;
+    if(mensaje.indexOf('@') !== -1 ) {
+         campo.style.borderBottomColor = 'green';
+         campo.classList.remove('error');
+    } else {
+         campo.style.borderBottomColor = 'red';
+         campo.classList.add('error');
+    }
+}
