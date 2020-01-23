87463514ee69e5c542099ce8de73dffef44e1657
Jorge García Gómez <jorge@jorgegarciagomez.es>
2020-01-23 20:28:10 +0100

    class Interfaz

--- CursoUdemyJS/cotizaSeguro/js/appes6.js
+++ CursoUdemyJS/cotizaSeguro/js/appes6.js
@@ -45,3 +45,58 @@ class Seguro{
     
     }
 }
+
+
+// Todo lo que se muestra
+class Interfaz{
+    mostrarMensaje(mensaje, tipo) {
+         const div = document.createElement('div');
+    
+         if(tipo === 'error') {
+              div.classList.add('mensaje','error');
+         } else {
+              div.classList.add('mensaje','correcto');
+         }
+         div.innerHTML = `${mensaje}`;
+         formulario.insertBefore(div, document.querySelector('.form-group'));
+    
+         setTimeout(function() {
+              document.querySelector('.mensaje').remove();
+         }, 3000);
+    } 
+    mostrarResultado(seguro, total) {
+         const resultado = document.getElementById('resultado');
+         let marca;
+         switch(seguro.marca) {
+              case '1':
+                   marca = 'Americano';
+                   break;
+              case '2':
+                   marca = 'Asiatico';
+                   break;
+              case '3':
+                   marca = 'Europeo';
+                   break;
+         }
+         // Crear un div
+         const div = document.createElement('div');
+         // Insertar la informacion
+         div.innerHTML = `
+              <p class='header'>Tu Resumen: </p>
+              <p>Marca: ${marca} </p>
+              <p>Año: ${seguro.anio} </p>
+              <p>Tipo: ${seguro.tipo} </p>
+              <p> Total: $ ${total} </p>
+         `;
+    
+         const spinner = document.querySelector('#cargando img');
+         spinner.style.display = 'block';
+         setTimeout(function() {
+              spinner.style.display = 'none';
+              resultado.appendChild(div);
+         }, 3000);
+         
+    
+    }
+    
+}
