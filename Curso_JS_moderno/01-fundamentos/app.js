2423e3345e340d94a41727a68ade0eb1f68f8f65
Jorge García Gómez <jorge@jorgegarciagomez.es>
2020-02-10 20:09:53 +0100

    console table

--- Curso_JS_moderno/01-fundamentos/app.js
+++ Curso_JS_moderno/01-fundamentos/app.js
@@ -1,2 +1,12 @@
-console.log(console.log("Hola Mundo"));
 
+let a = 10,
+    b = 20,
+    c = 30,
+    d = "Spiderman",
+    x =  a + b;
+
+
+
+
+console.log(console.log("Hola Mundo"));
+console.table({a,b,c,d,x});
